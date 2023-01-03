bb503af34ecc8b419e74e884c2f3671bc6044269
zhangkai <hnrzzk@163.com>
2023-01-03 15:25:18 +0800

    [MavenDemo]feat:add jar-with-dependency template; add execute shell template.

---
+++ MavenDemo/script/test.sh
@@ -0,0 +1,5 @@
+shell_path=$(dirname $0)
+echo "filePath: ${shell_path}"
+cd $shell_path
+
+pwd
---
+++ MavenDemo/src/main/java/priv/zhk/demo/MavenLaunch.java
@@ -0,0 +1,8 @@
+package priv.zhk.demo;
+
+public class MavenLaunch {
+    public static void main(String[] args) {
+        System.out.println("This is a maven demo.");
+    }
+}
+
