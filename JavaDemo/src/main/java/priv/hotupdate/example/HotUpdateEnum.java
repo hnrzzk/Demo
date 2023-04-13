92376b5ed7b12b06bca408febdfbc1c9cc47f20a
zhangkai <hnrzzk@163.com>
2023-04-13 15:51:11 +0800

    [HotUpdate]feat:add some comments.

--- JavaDemo/src/main/java/priv/hotupdate/example/HotUpdateEnum.java
+++ JavaDemo/src/main/java/priv/hotupdate/example/HotUpdateEnum.java
@@ -1,5 +1,8 @@
 package priv.hotupdate.example;
 
+/**
+ * 新增枚举无法通过Instrument热更。调用redefinition会抛出 class redefinition failed: attempted to change the schema异常
+ */
 public enum HotUpdateEnum {
     One,
     Two,
