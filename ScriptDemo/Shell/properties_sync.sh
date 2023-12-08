a36bde0d39a487842901a5ecfd9504d285286de0
zhangkai <hnrzzk@163.com>
2023-12-08 15:31:55 +0800

    [Shell]fix:优化属性同步脚本，避免因为不同操作系统的换行符导致的追加数据到当前文本末尾，而不是新启一行的问题。

--- ScriptDemo/Shell/properties_sync.sh
+++ ScriptDemo/Shell/properties_sync.sh
@@ -3,11 +3,11 @@ shell_path=$(dirname $0)
 echo "cd to filePath:${shell_path}"
 cd $shell_path
 
-source_file=./config/config.properties
-target_file=./BOOT-INF/classes/application.properties
+source_file="./config/config.properties"
+target_file="./BOOT-INF/classes/application.properties"
 
-dos2unix source_file
-dos2unix target_file
+dos2unix $source_file
+dos2unix $target_file
 # 读取config.properties文件 并忽略掉包含特殊符号的行
 grep -v '^[[:space:]]*$' ${source_file} | while IFS='=' read -r key value; do
   # 忽略注释行和空行
@@ -24,8 +24,10 @@ grep -v '^[[:space:]]*$' ${source_file} | while IFS='=' read -r key value; do
 
   # 在application.properties中替换或追加配置项
   if grep -q "^$key=" ${target_file}; then
+    echo "change prop key:$key value:$value"
     sed -i "s/^$key=.*/$key=$value/" ${target_file}
   else
-    echo "$key=$value" >> ${target_file}
+    echo "add prop key:$key value:$value"
+    sed -i "\$a\\$key=$value" ${target_file}
   fi
 done
